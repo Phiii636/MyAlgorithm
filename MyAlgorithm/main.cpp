d338f684c5d8ad59cc83f26d05a86a39e4104b54
Phiii636 <phiii636@outlook.com>
2025-01-06 01:18:39 +0800

    Function swap()

--- MyAlgorithm/main.cpp
+++ MyAlgorithm/main.cpp
@@ -2,15 +2,11 @@
 #include <vector>
 #include <string>
 
+#include <algorithm>
+
 int main()
 {
-	vector<int> nums{ 10,20 };
-	//int nums[2]{ 10,20 };
-
 	int a{ 10 }, b{ 20 };
-	my::swap<int>(a, b);
-
-	//my::swap<int>(nums.begin(), nums.begin()-1);
-	//cout << "nums[0] = " << nums[0] << endl;
-	//cout << "nums[1] = " << nums[1] << endl;
+	//cout << my::larger<int>(a, b,
+	//	[&](int& x, int& y) { return x >= y; }) << endl;
 }
--- MyAlgorithm/myAlgorithm.h
+++ MyAlgorithm/myAlgorithm.h
@@ -1,14 +1,23 @@
 #pragma once
 #include <iostream>
-#include <iterator>
+#include <vector>
 using namespace std;
 
 namespace my
 {
 	template<class T>
 	void swap(T& x, T& y);
+
 	template<class T>
-	void swap(iterator<T, T>& x, iterator<T, T>& y);
+	T& larger(T& x, T& y, bool compareFunc(T&, T&) = [&](T& _x, T& _y) { return _x >= _y; });
+	//template<class T>
+	//T& max(vector<T>& nums);
+
+	template<class T>
+	void sort(void sortFunc(vector<T>), vector<T>& nums, bool compareFunc(T&, T&));
+	template<class T>
+	void sort_select(vector<T>& nums,
+		bool compareFunc(T&, T&) = [&](T& x, T& y){ return x < y; });
 
 	//交换元素
 	template<class T>
@@ -19,10 +28,39 @@ namespace my
 		y = tmp;
 	}
 
+	//二者更大元素
 	template<class T>
-	void swap(iterator<T, T>& x, iterator<T, T>& y)
+	T& larger(T& x, T& y, bool compareFunc(T&, T&))
 	{
-		swap<T>(*x, *y);
+		if (compareFunc(x, y))
+			return x;
+		else
+			return y;
 	}
+
+	//自定义排序
+	template<class T>
+	void sort(void sortFunc(vector<T>), vector<T>& nums, bool compareFunc(T&, T&))
+	{
+
+	}
+
+	template<class T>
+	void sort_select(vector<T>& nums, bool compareFunc(T&, T&))
+	{
+		size_t left{};
+		size_t len = nums.size();
+
+		while (left < len - 1)
+		{
+			size_t right = left + 1;
+			T m{ nums[left] };
+			while (right < len)
+			{
+
+			}
+		}
+	}
+
 
 }
